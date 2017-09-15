078066832573d1253630f893d974b8824a76ca41
e <1176527598@qq.com>
2017-09-15 19:52:21 +0800

    1

---
+++ app/src/main/java/com/example/lakalaka/test1/Person.java
@@ -0,0 +1,26 @@
+package com.example.lakalaka.test1;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by lu17852172914 on 2017/9/15.
+ */
+
+public class Person extends BmobObject {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
