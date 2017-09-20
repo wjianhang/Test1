8050522f1f3707384f5d1537275863a38c08e2bf
e <1176527598@qq.com>
2017-09-20 19:23:00 +0800

    User_information

--- app/src/main/java/com/example/lakalaka/test1/User_information.java
+++ app/src/main/java/com/example/lakalaka/test1/User_information.java
@@ -9,6 +9,10 @@ import cn.bmob.v3.BmobObject;
 public class User_information extends BmobObject {
     private String user_name;
     private String user_password;
+    private String nickname;
+    private String user_sex;
+    private String signature;
+    private String pay_password;
 
 
     public String getName(){return user_name;}
@@ -21,7 +25,22 @@ public class User_information extends BmobObject {
     public void setPassword(String password) {
         this.user_password = password;
     }
-
+    public  String getNickname(){return nickname;}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public  String getUser_sex(){return user_sex;}
+    public void setUser_sex(String sex) {
+        this.user_sex = sex;
+    }
+    public  String getSignature(){return signature;}
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+    public  String getPay_password(){return pay_password;}
+    public void setPay_password(String pay_password) {
+        this.pay_password = pay_password;
+    }
 
 
 
